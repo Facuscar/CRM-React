8df439241014a4c51894914bf1a9ebc54571dc26
Facuscar <facundo.scardillo@dracoder.com>
2022-07-07 15:15:30 -0300

    Adding react router to the web, also navigation coloring according to the url

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,24 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link, useLocation } from 'react-router-dom';
 
 function Layout() {
+
+    const location = useLocation();
+    const url = location.pathname;
+    console.log(url);
+
     return (
-        <div>
-            <h1>From layout</h1>
-            <Outlet></Outlet>
+        <div className='md:flex md:min-h-screen'>
+            <div className='md:w-1/4 bg-blue-900 px-5 py-10'>
+                <h2 className='text-4xl font-black text-center text-white'>CRM - Clients</h2>
+                
+                <nav className='mt-10'>
+                    <Link to="/clients" className={` ${url === '/clients' ? 'text-blue-300' : 'text-white' } text-2xl block mt-2 hover:text-blue-300`}>Clients</Link>
+                    <Link to="/clients/new" className={` ${url === '/clients/new' ? 'text-blue-300' : 'text-white' } text-white text-2xl block mt-2 hover:text-blue-300`}>New client</Link>
+                </nav>
+            </div>
+            <div className='md:w-3/4'>
+                2
+            </div>
         </div>
         
      );
