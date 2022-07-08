e26c76aa5f1568791d3dee206ef23400f71d9722
Facuscar <facundo.scardillo@dracoder.com>
2022-07-08 18:01:14 -0300

    Add: initial values with formik

--- src/components/ClientForm.jsx
+++ src/components/ClientForm.jsx
@@ -1,40 +1,60 @@
 import { Formik, Form, Field } from 'formik';
 
 function ClientForm() {
+
+    const handleSubmit = (values) => {
+        console.log(values);
+    }
+
     return ( 
         <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
            <h1 className="text-gray-600 font-bold text-xl uppercase text-center">Add new cient</h1>
 
-           <Formik>
+           <Formik
+                initialValues={{
+                    name: 'Facu',
+                    company: '',
+                    email: '',
+                    phone: '',
+                    notes: '',
+                }}
+                onSubmit={(values) => {
+                    handleSubmit(values)
+                }}
+           >
+                {() => (
+
+                
                 <Form className='mt-10'>
 
                     <div className='mb-4'>
                         <label htmlFor="name" className='text-gray-800 mt-2'>Name:</label>
-                        <Field id="name" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's name"></Field>
+                        <Field name="name" id="name" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's name"></Field>
                     </div>
 
                     <div className='mb-4'>
                         <label htmlFor="company" className='text-gray-800 mt-2'>Company:</label>
-                        <Field id="company" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's company"></Field>
+                        <Field name="company" id="company" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's company"></Field>
                     </div>
 
                     <div className='mb-4'>
                         <label htmlFor="email" className='text-gray-800 mt-2'>Email:</label>
-                        <Field id="email" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's email"></Field>
+                        <Field name="email" id="email" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's email"></Field>
                     </div>
 
                     <div className='mb-4'>
-                        <label htmlFor="tel" className='text-gray-800 mt-2'>Phone:</label>
-                        <Field id="tel" type="tel" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's phone"></Field>
+                        <label htmlFor="phone" className='text-gray-800 mt-2'>Phone:</label>
+                        <Field name="phone" id="phone" type="tel" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's phone"></Field>
                     </div>
 
                     <div className='mb-4'>
                         <label htmlFor="notes" className='text-gray-800 mt-2'>Phone:</label>
-                        <Field id="notes" as="textarea" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's notes"></Field>
+                        <Field name="notes" id="notes" as="textarea" type="text" className="mt-2 block w-full p-3 bg-gray-50" placeholder="Client's notes"></Field>
                     </div>
 
                     <input type="submit"  value={"Add client"} className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"/>
                 </Form>
+                )}
             </Formik>
         </div>
 
