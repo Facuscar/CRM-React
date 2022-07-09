85abe9acd4e68350213460e40cec4458e75c0568
Facuscar <facundo.scardillo@dracoder.com>
2022-07-09 19:15:43 -0300

    Add: clear form after sending data

--- src/components/ClientForm.jsx
+++ src/components/ClientForm.jsx
@@ -45,8 +45,10 @@ function ClientForm() {
                     phone: '',
                     notes: '',
                 }}
-                onSubmit={(values) => {
-                    handleSubmit(values);
+                onSubmit={ async (values, {resetForm}) => {
+                    await handleSubmit(values);
+
+                    resetForm();
                 }}
                 validationSchema = {newClientSchema}
            >
