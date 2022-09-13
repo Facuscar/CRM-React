bc0349442cdefc8396b30d3cb989520adb5d8f87
Facuscar <facundo.scardillo@dracoder.com>
2022-09-13 01:39:03 -0300

    Add: render client details

--- src/pages/ClientInfo.jsx
+++ src/pages/ClientInfo.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 function ClientInfo() {
-
-    const params = useParams();
     
+    const [client, setClient] = useState({})
+
+    const { id } = useParams();
+
+    useEffect(() => {
+        const getAPIClient = async () => {
+            try {
+                const url = `http://localhost:4000/clients/${id}`
+                const res = await fetch(url)
+                const data = await res.json()
+                setClient(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getAPIClient();
+    }, []);
+    
+    const { name, company, email, phone, notes } = client
+
     return ( 
-        <h1>
-            clientInfo.jsx
-        </h1>
+        <div>
+            <h1 className='font-black text-4xl text-blue-900'>Client: {id}</h1>
+            <p className='text-2xl mt-10 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Name:</span> {name}</p>
+            <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Company:</span> {company}</p>
+            <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Email:</span> {email}</p>
+            {phone && <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Phone:</span> {phone}</p>}
+            {notes && <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Notes:</span> {notes}</p>}
+            
+        </div>
      );
 }
 
