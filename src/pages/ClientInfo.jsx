548f93b7f99b91b6f872a1e14bf63a9d39c3de05
Facuscar <facundo.scardillo@dracoder.com>
2022-09-13 01:49:17 -0300

    Add: 404 and loading state

--- src/pages/ClientInfo.jsx
+++ src/pages/ClientInfo.jsx
@@ -4,33 +4,47 @@ import { useParams } from 'react-router-dom';
 function ClientInfo() {
     
     const [client, setClient] = useState({})
+    const [loading, setLoading] = useState(false);
 
     const { id } = useParams();
 
     useEffect(() => {
+        setLoading(!loading);
         const getAPIClient = async () => {
             try {
                 const url = `http://localhost:4000/clients/${id}`
                 const res = await fetch(url)
                 const data = await res.json()
                 setClient(data);
+                
             } catch (error) {
                 console.log(error);
             }
+            setLoading(false)
         }
         getAPIClient();
     }, []);
     
     const { name, company, email, phone, notes } = client
+    console.log(client);
 
     return ( 
         <div>
-            <h1 className='font-black text-4xl text-blue-900'>Client: {id}</h1>
-            <p className='text-2xl mt-10 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Name:</span> {name}</p>
-            <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Company:</span> {company}</p>
-            <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Email:</span> {email}</p>
-            {phone && <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Phone:</span> {phone}</p>}
-            {notes && <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Notes:</span> {notes}</p>}
+            {loading ? <h1>Loading</h1> : 
+            <>
+            {
+                Object.keys(client).length === 0 ? <h1>404 Client not found</h1> : 
+                <> 
+                    <h1 className='font-black text-4xl text-blue-900'>Client: {id}</h1>
+                    <p className='text-2xl mt-10 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Name:</span> {name}</p>
+                    <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Company:</span> {company}</p>
+                    <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Email:</span> {email}</p>
+                    {phone && <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Phone:</span> {phone}</p>}
+                    {notes && <p className='text-2xl mt-4 text-gray-600'><span className='text-gray-800 uppercase font-bold' >Notes:</span> {notes}</p>}
+                </>
+            }
+            </> 
+        }
             
         </div>
      );
